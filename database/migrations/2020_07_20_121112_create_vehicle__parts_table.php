7893169285d97d27310f870ba110005d394060c5
vagrant <vagrant@homestead>
2020-08-05 13:59:05 +0000

    migration change vehicle parts

--- database/migrations/2020_07_20_121112_create_vehicle__parts_table.php
+++ database/migrations/2020_07_20_121112_create_vehicle__parts_table.php
@@ -18,11 +18,17 @@ class CreateVehiclePartsTable extends Migration
             $table->string('name');
             $table->integer('stock');
             $table->float('price');
-            $table->string('photo');        
+            $table->string('photo'); 
+
+            $table->integer('user_id')->unsigned();       
             $table->foreign('user_id')->references('id')->on('users')
                 ->onDelete('cascade');
+
+            $table->integer('brand_id')->unsigned();
             $table->foreign('brand_id')->references('id')->on('brands')
                 ->onDelete('cascade');
+            
+            $table->integer('vehicletype_id')->unsigned();
             $table->foreign('vehicletype_id')->references('id')->on('vehicle_types')
                 ->onDelete('cascade');
             $table->timestamps();
