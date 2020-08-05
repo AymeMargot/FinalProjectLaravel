1535d2b5e9d83dd6d4009d4022de46b3e6339435
vagrant <vagrant@homestead>
2020-08-05 14:33:24 +0000

    migration change setting

--- database/migrations/2020_07_13_131729_create_settings_table.php
+++ database/migrations/2020_07_13_131729_create_settings_table.php
@@ -19,6 +19,8 @@ class CreateSettingsTable extends Migration
             $table->time('timeEnd');
             $table->integer('maxBooking');
             $table->integer('maxService');
+
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')
                 ->onDelete('cascade');
             $table->timestamps();
