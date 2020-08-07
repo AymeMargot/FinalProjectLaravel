76ca27d37bcf3e32441ee77c8b25ca733f452557
vagrant <vagrant@homestead>
2020-08-07 12:21:31 +0000

    home.blade fixing fonts size

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <meta name="csrf-token" content="{{ csrf_token() }}">  
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>@yield('title')</title>
   <meta content="" name="descriptison">
@@ -24,28 +24,29 @@
   <link href="/vendor/venobox/venobox.css" rel="stylesheet">
   <link href="/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
   <link href="/vendor/aos/aos.css" rel="stylesheet">
-<!-- tool tip -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<!-- end tip -->
-<!-- Template Main CSS File -->.
-<link href="{{ asset('/css/style.css') }}" rel="stylesheet">
-<link href="{{ asset('/css/table.css') }}" rel="stylesheet">
+  <!-- tool tip -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- end tip -->
+  <!-- Template Main CSS File -->.
+  <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+  <link href="{{ asset('/css/table.css') }}" rel="stylesheet">
 </head>
-<body>  
+
+<body>
   @include('layouts.header')
   @yield('content')
   @include('layouts.footer')
   <!-- tol tip -->
-  
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<!-- end tip-->
+
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <!-- end tip-->
   <a href="#" class="back-to-top"><i class="bx bxs-up-arrow-alt"></i></a>
-   <!-- Vendor JS Files -->
+  <!-- Vendor JS Files -->
   <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
   <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <script src="{{ asset('vendor/jquery.easing/jquery.easing.min.js') }}"></script>
@@ -57,7 +58,13 @@
 
   <!-- Template Main JS File -->
   <script src="{{ asset('js/main.js') }}"></script>
-
+  <script type="text/javascript">
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      }
+    });
+  </script>
 
 </body>
 
